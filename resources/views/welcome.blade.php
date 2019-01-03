0eb0a9d9efaaf855b798593ce28b9a11e7f83628
diwuh <1581176417@qq.com>
2019-01-03 20:37:08 +0800

    第一次

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<img src="http://yuwenb.oss-cn-beijing.aliyuncs.com/huixuan/images/web.jpg?Expires=1546347177&OSSAccessKeyId=TMP.AQFW9qeV89QuGds3-ieI3Lai3mHY6N-zjGD7jQiDEFbaPQV2MT7ewNDRfUDAADAtAhQpXQygAUSOvn4U26wlN4lflCm9yAIVAMgEESkY3hNmpI-2bGHWpOBVoMD3&Signature=HTwPjFDEMlvQvUuKhYeTH2ssICk%3D" alt="">
+</body>
+</html>
